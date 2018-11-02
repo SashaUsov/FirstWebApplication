8a8a9b84c659f80f352b60ee1c1dcce80dfaa457
Oleksandr Samsonov <oleksandr_samsonov@epam.com>
2018-11-02 19:34:08 +0200

    Simple message list

--- src/main/java/sashaVosu/firstWebApplication/controller/MainController.java
+++ src/main/java/sashaVosu/firstWebApplication/controller/MainController.java
@@ -1,15 +1,32 @@
 package sashaVosu.firstWebApplication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("main")
 public class MainController {
-    @GetMapping
-    public String main() {
 
-        return "Simple web application";
+    private List<String> messageList = new ArrayList<String>(){{
+        add("First application message");
+        add("Second application message");
+    }};
+
+    @GetMapping
+    private List<String> main() {
+
+        return messageList;
+    }
+
+    @PostMapping
+    public String createMessage(@RequestBody String messageText) throws IndexOutOfBoundsException{
+        if (messageText.length() > 1 && messageText.length() <= 140) {
+            messageList.add(messageText);
+            return messageText;
+        }  else {
+            throw new IndexOutOfBoundsException();
+        }
     }
 }
